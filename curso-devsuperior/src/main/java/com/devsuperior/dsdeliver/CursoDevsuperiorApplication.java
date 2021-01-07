02a1a1667db600b2b0c6f3557637b380698d80d3
Yhann Carvalho <yhann.carvalho>
2021-01-06 22:51:26 -0300

    Projeto criado

---
+++ curso-devsuperior/src/main/java/com/devsuperior/dsdeliver/CursoDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDevsuperiorApplication.class, args);
+	}
+
+}
